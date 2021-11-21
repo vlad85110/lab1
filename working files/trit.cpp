336ba20a2183fc89a29e4415b48c7760719b9c40
vlad85110 <vk1010200>
2021-11-21 16:06:50 +0700

    changes

--- working files/trit.cpp
+++ working files/trit.cpp
@@ -2,6 +2,10 @@
 
 
 namespace custom {
+    size_t round_up(size_t x, size_t y) {
+        return x / y + ((x % y != 0) ? 1 : 0);
+    }
+
     Tritset::Tritset(size_t size) {
         this->size = round_up(size * 2, 8 * sizeof(uint));
         data = new uint [this->size];
@@ -16,9 +20,14 @@ namespace custom {
         return size * sizeof(uint) * 8 / 2;
     }
 
-    size_t round_up(size_t x, size_t y)
-    {
-         return x / y + ((x % y != 0) ? 1 : 0);
+    void Tritset::clear_trit(size_t index, size_t pos) {
+        uint cleaner = ~(3 << pos);
+        data[index] &= cleaner;
+    }
+
+    void Tritset::set_trit(size_t index, size_t pos, size_t value) {
+        clear_trit(index, pos);
+        data[index] = value << pos;
     }
 }
 
--- working files/trit.h
+++ working files/trit.h
@@ -9,51 +9,13 @@ namespace custom {
         False, Unknown, True
     };
 
+    class TritProxy;
+
     class Tritset {
     public:
         uint *data;
         size_t size;
-        class TritProxy {
-        private:
-            Tritset &origin;
-            size_t index;
-            size_t position;
-        public:
-            TritProxy(Tritset &origin, size_t index) : origin(origin) {
-                this->origin = origin;
-                this->index = index * 2 / 8 / sizeof(uint);
-                this->position =  32 - (index % (sizeof(uint) * 8 / 2));
-            }
-
-            ~TritProxy() {
-                std::cout << "destructor" << std::endl;
-            }
-
-            TritProxy & operator=(uint value) {
-                if (index >= origin.capacity()) {
-                    if (value != Unknown)
-                        /*realloc */
-                        write(value);
-                }
-                else
-                    write(value);
-                /*not ended*/
-                return *this;
-            }
-
-            void write (size_t value) {
-                origin.set_trit(index, position, value);
-            }
-
-            void read () {
-                origin.get_trit(index, position);
-                origin.clear_trit(index, position);
-            }
-
-            operator Trit() {
-                return origin.get_trit(index, position);
-            }
-        };
+        friend class TritProxy;
     public:
         explicit Tritset(size_t length);
 
@@ -61,20 +23,15 @@ namespace custom {
 
         size_t capacity() const;
 
-        TritProxy operator[](int index) {
+        /*TritProxy operator[](int index) {
             TritProxy temp(*this, index);
             return temp;
-        }
+        }*/
 
-        void clear_trit (size_t index, size_t pos) {
-            uint cleaner = ~(3 << pos);
-            data[index] &= cleaner;
-        }
+        void clear_trit (size_t index, size_t pos);
 
-        void set_trit(size_t index, size_t pos, size_t value) {
-            clear_trit(index, pos);
-            data[index] = value << pos;
-        }
+
+        void set_trit(size_t index, size_t pos, size_t value);
 
         Trit get_trit(size_t index, size_t pos) {
             uint getter = 3 << pos;
@@ -87,5 +44,47 @@ namespace custom {
             return get_trit(1,2);
         }
         // &= ~(000000000000000011 << pos)
+    };
+    class TritProxy {
+    private:
+        Tritset &origin;
+        size_t index;
+        size_t position;
+    public:
+        TritProxy(Tritset &origin, size_t index) : origin(origin) {
+            this->origin = origin;
+            this->index = index * 2 / 8 / sizeof(uint);
+            this->position =  32 - (index % (sizeof(uint) * 8 / 2));
+        }
+
+        ~TritProxy() {
+            std::cout << "destructor" << std::endl;
+        }
+
+        TritProxy & operator=(uint value) {
+            if (index >= origin.capacity()) {
+                if (value != Unknown)
+                    /*realloc */
+                    write(value);
+            }
+            else
+                write(value);
+            /*not ended*/
+            return *this;
+        }
+
+        void write (size_t value) {
+            origin.set_trit(index, position, value);
+        }
+
+        void read () {
+            origin.get_trit(index, position);
+            origin.clear_trit(index, position);
+        }
+
+        
+        operator Trit() {
+            return origin.get_trit(index, position);
+        }
     };
 }
