c377a7eeb8de988b644584b29eef7c3f7d6564b2
vlad85110 <vk1010200>
2021-11-23 22:55:11 +0700

    start testing

--- working files/main.cpp
+++ working files/main.cpp
@@ -1,5 +1,6 @@
 #include "func.h"
 #include "trit.h"
+#include <gtest/gtest.h>
 
 
 using namespace std;
@@ -9,33 +10,41 @@ using custom::False;
 using custom::Trit;
 
 
-int main() {
-
-    custom::Tritset set(1000);
-// length of internal array
-    size_t allocLength = set.capacity();
-    assert(allocLength >= 1000*2 / 8 / sizeof(uint) );
-// 1000*2 - min bits count
-// 1000*2 / 8 - min bytes count
-// 1000*2 / 8 / sizeof(uint) - min uint[] size
-
-//не выделяет никакой памяти
-    set[1000000000] = Unknown;
-    assert(allocLength == set.capacity());
-
-// false, but no exception or memory allocation if(set[2000000000]==True){}
-    assert(allocLength == set.capacity());
-
-//выделение памяти
-    set[1000000000] = True;
-    assert(allocLength < set.capacity());
-
-//no memory operations
-    allocLength = set.capacity();
-    set[1000000000] = Unknown;
-    set[1000000] = False;
-    assert(allocLength == set.capacity());
-    set.shrink();
-    assert(allocLength > set.capacity());
+TEST(Round_test, equal) {
+    EXPECT_EQ(3, custom::round_up(5,2));
+    EXPECT_EQ(4, custom::round_up(10,3));
+    EXPECT_EQ(7, custom::round_up(40,6));
+    EXPECT_EQ(8, custom::round_up(15,2));
 }
+
+TEST(conversion_test, u_t_t) {
+    EXPECT_EQ(0, custom::uint_to_trit(0));
+    EXPECT_EQ(16, custom::uint_to_trit(1));
+    EXPECT_EQ(32, custom::uint_to_trit(2));
+    EXPECT_EQ(48, custom::uint_to_trit(3));
+}
+
+TEST(conversion_test, t_t_u){
+    EXPECT_EQ(0, custom::trit_to_uint(0));
+    EXPECT_EQ(0, custom::trit_to_uint(12));
+
+    EXPECT_EQ(1, custom::trit_to_uint(16));
+    EXPECT_EQ(1, custom::trit_to_uint(25));
+
+    EXPECT_EQ(2, custom::trit_to_uint(32));
+    EXPECT_EQ(2, custom::trit_to_uint(33));
+}
+
+class TritsetTest : public ::testing::Test {
+protected:
+    custom::Tritset _t1;
+};
+
+TEST_F(TritsetTest, constructor_test) {
+
+}
+
+/*int main() {
+   return 0;
+}*/
                     
